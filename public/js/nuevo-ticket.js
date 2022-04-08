ef1e4c40dad2c39825bfea970776b0c7ebfdb0f8
carlosbarondev <carlosbarondev@gmail.com>
2022-04-08 12:42:52 +0200

    Updated controller

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,23 @@
+// Referencias HTML
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnCrear = document.querySelector('button');
 
+const socket = io();
 
-console.log('Nuevo Ticket HTML');
+socket.on('connect', () => {
+    btnCrear.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnCrear.disabled = true;
+});
+
+socket.on('last-ticket', (last) => {
+    lblNuevoTicket.innerText = 'Ticket ' + last;
+});
+
+btnCrear.addEventListener('click', () => {
+    socket.emit('next-ticket', null, (ticket) => {
+        lblNuevoTicket.innerText = ticket;
+    });
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,22 +1,14 @@
-const ticketControl = require("../models/ticketControl");
+const TicketControl = require("../models/ticketControl");
 
-const ticket_Control = new ticketControl();
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
 
-    console.log('Cliente conectado', socket.id);
+    socket.emit('last-ticket', ticketControl.last);
 
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id);
-    });
-
-    socket.on('enviar-mensaje', (payload, callback) => {
-
-        const id = 123456789;
-        callback(id);
-
-        socket.broadcast.emit('enviar-mensaje', payload);
-
+    socket.on('next-ticket', (payload, callback) => {
+        const next = ticketControl.next();
+        callback(next);
     })
 
 }
